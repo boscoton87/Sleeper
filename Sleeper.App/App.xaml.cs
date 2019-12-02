23a18deda28e515ce8abdaa2de8468621a729d33
Poseidon\Alex <alex.l.harper@outlook.com>
2019-12-02 09:02:07 -0600

    Initial Commit.

---
+++ Sleeper.App/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Facade.Services;
+using Sleeper.Core.Helpers;
+using Sleeper.Core.Interfaces;
+using Sleeper.Core.Services;
+using System;
+using System.Windows;
+
+namespace Sleeper.App
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            Action sleepAction = () => WindowsSystemHelpers.PerformSleep();
+            Container.RegisterGlobalInstance<IDelayedActionService>(new DelayedActionService(sleepAction));
+        }
+    }
+}
---
+++ Sleeper.Core/Helpers/WindowsSystemHelpers.cs
@@ -0,0 +1,18 @@
+﻿namespace Sleeper.Core.Helpers
+{
+    public static class WindowsSystemHelpers
+    {
+        public static void PerformSleep()
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo()
+            {
+                WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden,
+                FileName = "rundll32.exe",
+                Arguments = "powrprof.dll,SetSuspendState 0,1,0",
+            };
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+    }
+}
