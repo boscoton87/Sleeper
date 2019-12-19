ded5ed3ad2b1cdd14926f0862043bc6dea82709b
Poseidon\Alex <alex.l.harper@outlook.com>
2019-12-18 23:13:02 -0600

    Updated styling and added logic to carry numbers between hours and minutes.  Updated Minute toggle buttons to increment by 15.

--- Sleeper.App/Controls/TimeDisplay.xaml.cs
+++ Sleeper.App/Controls/TimeDisplay.xaml.cs
@@ -1,6 +1,6 @@
-﻿using Sleeper.Core.Interfaces;
-using System;
+﻿using System;
 using System.ComponentModel;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
@@ -11,15 +11,26 @@ namespace Sleeper.App.Controls
     /// </summary>
     public partial class TimeDisplay : UserControl, INotifyPropertyChanged
     {
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         public int? NumberValue { get; set; } = 0;
+
+        public int IncrementValue { get; set; } = 1;
+
+        public int MaximumValue { get; set; } = 59;
 
         public int UnitsToMinutes { get; set; }
 
         public string Units { get; set; }
 
         public Action<int> ApplyDelay { get; set; }
+
+        public Func<int, bool> TryCarryToNextPosition { get; set; } = (value) => { return true; };
+
+        public Func<int, int?> TakeFromNextPosition { get; set; } = (value) => { return value; };
+
+        public Visibility ButtonVisibility { get; set; }
 
         public string DisplayTextBox
         {
@@ -42,6 +53,7 @@ namespace Sleeper.App.Controls
                 OnPropertyChanged("DisplayTextBox");
             }
         }
+
         public TimeDisplay()
         {
             InitializeComponent();
@@ -55,25 +67,17 @@ namespace Sleeper.App.Controls
 
         private void IncrementDisplayValue(object sender, MouseEventArgs e)
         {
-            if(NumberValue >= 99)
-            {
-                return;
-            }
-            var offset = IncrementDelayCount(UnitsToMinutes, NumberValue * UnitsToMinutes) / UnitsToMinutes;
+            var offset = IncrementDelayCount(UnitsToMinutes * IncrementValue) / UnitsToMinutes;
             ApplyOffset(offset);
         }
 
         private void DecrementDisplayValue(object sender, MouseEventArgs e)
         {
-            if (NumberValue <= 0)
-            {
-                return;
-            }
-            var offset = IncrementDelayCount((-1 * UnitsToMinutes), NumberValue * UnitsToMinutes) / UnitsToMinutes;
+            var offset = IncrementDelayCount(-1 * UnitsToMinutes * IncrementValue) / UnitsToMinutes;
             ApplyOffset(offset);
         }
 
-        private void ApplyOffset(int offset)
+        public void ApplyOffset(int offset)
         {
             if (!NumberValue.HasValue)
             {
@@ -82,15 +86,29 @@ namespace Sleeper.App.Controls
             else
             {
                 NumberValue += offset;
+                if (NumberValue > MaximumValue)
+                {
+                    var valueToCarry = NumberValue.Value / (MaximumValue + 1);
+                    var remainingValue = NumberValue.Value % (MaximumValue + 1);
+                    NumberValue = remainingValue;
+                    if (!TryCarryToNextPosition(valueToCarry))
+                    {
+                        NumberValue = MaximumValue;
+                    }
+                }
+                else if (NumberValue < 0)
+                {
+                    var takenValue = TakeFromNextPosition(1);
+                    NumberValue = takenValue.HasValue ? NumberValue + (takenValue * (MaximumValue + 1)) : 0;
+                }
             }
             OnPropertyChanged("DisplayTextBox");
         }
 
-        private int IncrementDelayCount(int delayInMinutes, int? referenceValue)
+        private int IncrementDelayCount(int delayInMinutes)
         {
-            var offset = (referenceValue + delayInMinutes) < 0 ? 0 : delayInMinutes;
-            ApplyDelay(offset);
-            return offset;
+            ApplyDelay(delayInMinutes);
+            return delayInMinutes;
         }
     }
 }
